25b2076254f11873d129ab2393c5a57315d03513
Duico <a.duico@student.tudelft.nl>
2022-06-01 23:27:04 +0200

    WIP Bayesian optimization

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from logging.config import dictConfig
 import gym
 import torch
 import matplotlib
@@ -7,7 +8,6 @@ from wrappers.cartpole_v1_wrapper import CartPoleV1AngleEnergyRewardWrapper
 from config import default_params
 from dqn import DQN
 from replay_buffer import ReplayBuffer
-import nobo, skopt
 from wrappers.mountaincar_discrete_wrapper import DiscreteMountainCarNormal
 from experiment import Experiment
 from config import default_params
@@ -23,9 +23,9 @@ config_params["k"] = 5
 
 # env = DiscreteMountainCar3Distance(gym.make("MountainCar-v0"))
 # env = PendulumRewardWrapper(gym.make("Pendulum-v1")) #this one doesn't work yet, because it has no env.action_space.n
-# env = CartPoleV1AngleEnergyRewardWrapper(gym.make("CartPole-v1"))
+env = CartPoleV1AngleEnergyRewardWrapper(gym.make("CartPole-v1"))
 
-env = DiscreteMountainCarNormal(gym.make("MountainCar-v0"))
+# env = DiscreteMountainCarNormal(gym.make("MountainCar-v0"))
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -37,8 +37,6 @@ env_params = {
     "reward_names": (env.__dict__["reward_names"], torch.StringType),
     "dones": ((1,), torch.bool),
 }
-
-preference = np.array([1.0], dtype=np.single)
 
 model = torch.nn.Sequential(
     torch.nn.Linear(env_params["states"][0][0] + env_params["rewards"][0][0], 215),
@@ -55,47 +53,80 @@ model = torch.nn.Sequential(
 )
 
 
-def bayes_opt(input, output):
-    bo = nobo.Optimizer([skopt.space.Real(0, 1), skopt.space.Real(0, 1)])
-    for index in range(len(input)):
-        x = input[index]
-        y = output[index][0]
-        bo.tell(x, y)
-    x, lo, y, hi = bo.get_best()
-    return np.array(x, dtype=np.single)
+# TODO restructure code
+from bayes_opt import BayesianOptimization, UtilityFunction
+from sklearn.gaussian_process.kernels import Matern
+
+optimizer = BayesianOptimization(
+    f=None,
+    kernel=Matern(length_scale_bounds="fixed", nu=2.5),
+    pbounds={"x": (0, 1)},
+    verbose=2,
+    random_state=1,
+)
+optimizer.set_gp_params(alpha=1.0)
+utility = UtilityFunction(kind="ucb", kappa=2.5, xi=0.1)
+
+config_params["max_steps"] = int(1e4)
 
 
-preferences = []
-criterions = []
-for i in range(100):
-    if i >= 5:
-        preference = bayes_opt(preferences, criterions)
-    learner = DQN(model, config_params, device, env)
-    buffer = ReplayBuffer(
-        env_params, buffer_size=int(1e5), device=device, k=config_params.get("k", 1)
-    )
+learner = DQN(model, config_params, device, env)
+state_size = 4
+rnd = RNDUncertainty(400, input_dim=state_size, device=device)
+buffer = ReplayBuffer(
+    env_params, buffer_size=int(1e5), device=device, k=config_params.get("k", 1)
+)
+
+import matplotlib.pyplot as plt
+
+
+def plot_bo(bo):
+    x = np.linspace(-2, 10, 10000)
+    mean, sigma = bo._gp.predict(x.reshape(-1, 1), return_std=True)
+    plt.figure(figsize=(16, 9))
+    # plt.plot(x, f(x))
+    plt.plot(x, mean)
+    plt.fill_between(x, mean + sigma, mean - sigma, alpha=0.1)
+    plt.scatter(bo.space.params.flatten(), bo.space.target, c="red", s=50, zorder=10)
+    plt.show()
+
+
+def reduce_dim(x):
+    return x[:-1]
+
+
+def add_dim(x: dict):
+    l = []
+    for _, it in x.items():
+        l.append(it)
+    l = np.array(l)
+    sum = np.sum(l)
+    return np.concatenate((l, [1.0 - sum]), dtype=np.single)
+
+
+global_rewards = []
+
+continueExperiments = True
+while continueExperiments:
+    next_preference_proj = optimizer.suggest(utility)
+    # TODO make preferences work with dim!=2
+    next_preference = add_dim(next_preference_proj)
+    print("Next preference to probe is:", next_preference)
     experiment = Experiment(
         learner=learner,
         buffer=buffer,
         env=env,
         reward_dim=env_params["rewards"][0][0],
-        preference=preference,
+        preference=next_preference,
         params=config_params,
         device=device,
+        uncertainty=rnd,
     )
     experiment.run()
-    preferences.append(preference)
-    criterions.append(experiment.global_rewards)
-learner = DQN(model, config_params, device, env)
-rnd = RNDUncertainty(400, 2, device)
-experiment = Experiment(
-    learner=learner,
-    buffer=buffer,
-    env=env,
-    reward_dim=env_params["rewards"][0][0],
-    preference=preference,
-    params=config_params,
-    device=device,
-    uncertainty=rnd,
-)
-experiment.run()
+    global_rewards_experiment = experiment.global_rewards
+    metric = np.average(global_rewards_experiment)
+    global_rewards.append(metric)
+    optimizer.register(params=next_preference_proj, target=metric)
+    optimizer.suggest(utility)
+    plot_bo(optimizer)
+
