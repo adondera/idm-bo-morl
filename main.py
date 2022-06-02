a024d77b35db83fe696025b2a2c00f1ba9c995e9
Alin Dondera <a.dondera@student.tudelft.nl>
2022-06-02 12:05:54 +0200

    add sparse cartpole

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import torch
 import matplotlib
 import numpy as np
 
-from wrappers.cartpole_v1_wrapper import CartPoleV1AngleEnergyRewardWrapper
+from wrappers.cartpole_v1_wrapper import CartPoleV1AngleEnergyRewardWrapper, SparseCartpole
 from wrappers.mountaincar_discrete_wrapper import DiscreteMountainCarNormal, DiscreteMountainCarVelocity
 from replay_buffer import ReplayBuffer
 from SO_RL.replay_buffer_SO import ReplayBuffer_SO
@@ -22,10 +22,11 @@ config_params['max_steps'] = int(2E5)
 config_params['intrinsic_reward'] = True
 config_params['k'] = 5
 config_params['grad_repeats'] = 1
+config_params['multi_objective'] = False
 
 # env = DiscreteMountainCar3Distance(gym.make("MountainCar-v0"))
 # env = PendulumRewardWrapper(gym.make("Pendulum-v1")) #this one doesn't work yet, because it has no env.action_space.n
-env = CartPoleV1AngleEnergyRewardWrapper(gym.make("CartPole-v1"))
+env = SparseCartpole(gym.make("CartPole-v1"))
 
 # env = DiscreteMountainCarVelocity(gym.make("MountainCar-v0"))
 
@@ -34,9 +35,11 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 env_params = {
     "states": (env.observation_space.shape, torch.float32),
     "actions": ((1,), torch.long),
-    "rewards": ((1,), torch.float32),
+    "rewards": ((2,), torch.float32),
     "reward_names": (env.__dict__["reward_names"], torch.StringType),
-    "dones": ((1,), torch.bool)
+    "preferences": ((env.__dict__["numberPreferences"],), torch.float32),
+    "dones": ((1,), torch.bool),
+
 }
 
 preference = np.array([1.0, 0.0], dtype=np.single)
@@ -52,11 +55,11 @@ if config_params['multi_objective']:
 
     learner = DQN(model, config_params, device, env)
     buffer = ReplayBuffer(env_params, buffer_size=int(1e5), device=device, k=config_params.get('k', 1))
-    rnd = RNDUncertainty(400, env_params['states'][0][0], device)
+    rnd = RNDUncertainty(0, env_params['states'][0][0], device)
     experiment = Experiment(learner=learner, buffer=buffer, env=env, reward_dim=env_params["rewards"][0][0],
                             preference=preference, params=config_params, device=device, uncertainty=rnd)
 
-else: # for running single objective experiment
+else:  # for running single objective experiment
     model = torch.nn.Sequential(
         torch.nn.Linear(env_params['states'][0][0], 215), torch.nn.ReLU(),
         torch.nn.Linear(215, 512), torch.nn.ReLU(),
@@ -67,6 +70,7 @@ else: # for running single objective experiment
 
     learner = DQN_SO(model, config_params, device, env)
     buffer = ReplayBuffer_SO(env_params, buffer_size=int(1e5), device=device)
-    rnd = RNDUncertainty(400, env_params['states'][0][0], device)
-    experiment = Experiment_SO(learner=learner, buffer=buffer, env=env, params=config_params, device=device, uncertainty=rnd)
+    rnd = RNDUncertainty(40, env_params['states'][0][0], device)
+    experiment = Experiment_SO(learner=learner, buffer=buffer, env=env, params=config_params, device=device,
+                               uncertainty=rnd)
 experiment.run()
--- wrappers/cartpole_v1_wrapper.py
+++ wrappers/cartpole_v1_wrapper.py
@@ -55,3 +55,38 @@ class CartPoleV1AngleEnergyRewardWrapper(gym.RewardWrapper):
         self.previous_state = state
         return R, z
         # Returns (tuple of multi-objective rewards), z reward
+
+
+class SparseCartpole(gym.Wrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.total_steps = 0
+        self.previous_state = None
+
+        self.numberPreferences = 2
+        self.reward_names = ["1-angle", "-energy"]
+
+    def step(self, action):
+        observation, reward, done, info = self.env.step(action)
+        self.total_steps += 1
+        return observation, self.reward(reward), done, info
+
+    def reward(self, reward):
+        base_env = self.env.env
+        state = base_env.state
+        previous_state = state if self.previous_state is None else self.previous_state
+        position, velocity, angle, angular_velocity = state
+        position_0, velocity_0, angle_0, angular_velocity_0 = previous_state
+        delta_velocity = velocity - velocity_0
+        energy = pow(delta_velocity, 2)
+        R = (- abs(angle), -energy)
+
+        z = 0 if self.total_steps < 200 else 1
+        if z == 1:
+            print(z)
+        self.previous_state = state
+        return R, z
+
+    def reset(self, **kwargs):
+        self.total_steps = 0
+        return self.env.reset(**kwargs)
