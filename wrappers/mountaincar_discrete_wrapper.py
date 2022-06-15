e00aa49808ccd3e4752d210c3d36032ef6a24b48
Alin Dondera <a.dondera@student.tudelft.nl>
2022-06-15 21:51:25 +0200

    Add environment for mountain car

--- wrappers/mountaincar_discrete_wrapper.py
+++ wrappers/mountaincar_discrete_wrapper.py
@@ -40,14 +40,20 @@ class DiscreteMountainCarVelocity(RescaledEnv):
         return (current_velocity,), reward
 
 
-class DiscreteMountainCarNormal(RescaledEnv):
+class DiscreteMountainCarVelocityDistance(RescaledEnv):
     def __init__(self, env, max_episode_length=None):
         super().__init__(env, max_episode_length)
-        self.numberPreferences = 1
-        self.reward_names = ["Global reward"]
+        self.numberPreferences = 2
+        self.reward_names = ["Velocity", "-Distance to goal"]
 
-    def reward(self, reward: float) -> tuple[tuple[float,], float]:
-        return (reward,), reward
+    def reward(self, reward: float) -> tuple[tuple[float, float], float]:
+        goal_position = self.env.unwrapped.goal_position
+        start_position = -0.5
+        current_position, current_velocity = self.env.unwrapped.state
+        distance_right_to_start = abs(goal_position - start_position)
+        distance_to_right_hill = abs(current_position - goal_position)
+        distance_metric = min(distance_right_to_start, distance_to_right_hill)
+        return (abs(current_velocity), -distance_metric), reward
 
 # Use this code to play the environment or for debugging purposes
 
